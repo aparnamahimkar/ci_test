ceec5f6611fa6dcf7ccf32b23984109c0d0e951e
Aparna Mahimkar <aparna2m@gmail.com>
2017-02-15 12:34:44 +0530

    added regex

---
+++ regex.php
@@ -0,0 +1,9 @@
+<?php
+$received_date = "Jan 25, 2017";
+echo preg_match("/[a-zA-Z]{3}(\s)(\d{1,2})(\,\s)(\d{4})/",$received_date);
+
+//preg_match("/[A-Za-z]{3,3}\s[0-9]{1,2},\s[0-9]{4}/",$received_date);
+//[a-zA-Z]{3}(s\,)(\d{1,2})(s\,)(\d{4}) 
+exit;
+$str = " Patriotic ";
+echo "***".trim($str)."***";
